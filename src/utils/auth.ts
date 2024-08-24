89d006b2714e252325f7a4f52983685b2df0beaa
Jason Korol <ejkorol@gmail.com>
2024-08-24 18:46:37 -0400

    change hash functions for async await compatability, create function to issue jwts

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,20 +1,77 @@
 import bcrypt from "bcrypt";
+import * as jose from "jose";
+import { User } from "@prisma/client";
 
 const saltRounds = 10;
 
-export const hashPassword = (password: string) => {
-  bcrypt.genSalt(saltRounds, (err, salt) => {
-    if (err) return null;
-    bcrypt.hash(password, salt, (err, hash) => {
-      if (err) return null;
-      return hash;
-    });
-  });
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET has not been configured");
 };
 
-export const compareHash = (password: string, hash: string) => {
-  bcrypt.compare(password, hash, (err, result) => {
-    if (err) return null;
-    return result;
-  });
+// Hash Function
+export const hashPassword = async (password: string): Promise<string> => {
+  try {
+    const salt = await new Promise<string>((resolve, reject) => {
+      bcrypt.genSalt(saltRounds, (err, salt) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(salt);
+        };
+      });
+    });
+
+    return await new Promise<string>((resolve, reject) => {
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(hash);
+        };
+      });
+    });
+  } catch (e) {
+    console.error(e);
+    throw e;
+  };
+};
+
+// Compare Hashes
+export const compareHash = async (password: string, hash: string): Promise<boolean> => {
+  try {
+    return await new Promise<boolean>((resolve, reject) => {
+      bcrypt.compare(password, hash, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        };
+      });
+    });
+  } catch (e) {
+    console.error(e);
+    throw e;
+  };
+};
+
+// Issue JWT
+export const issueJWT = async (user: User) => {
+  try {
+
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+    const token = await new jose.SignJWT({
+      userId: user.id,
+      email: user.email,
+      user: user.first_name
+    })
+    .setProtectedHeader({ alg: "HS256" })
+    .setExpirationTime("720h")
+    .sign(secret);
+
+    return token;
+  } catch(e) {
+    console.error(e);
+    throw e;
+  };
 };
